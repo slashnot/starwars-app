3dde3a1f40f349af70eac387c197d6b7276ad26d
Ramkumar K <slashnot84@gmail.com>
2021-01-01 23:52:26 +0530

    Auth redirects fixed

--- src/components/auth/PrivateRoute.js
+++ src/components/auth/PrivateRoute.js
@@ -1,23 +1,20 @@
 // Router wrapper component to handle private routes
-import { Redirect, Route, useHistory } from 'react-router-dom'
-import { useContext, useEffect } from 'react';
+import { Route, useHistory } from 'react-router-dom'
 import AppContext from 'store/AppContext';
+import { useContext, useEffect } from 'react';
 
 const PrivateRoute = ({ component: Component, redirect = "/", ...rest }) => {
-    const isLoggedIn = localStorage.getItem('isLoggedIn') ? JSON.parse(localStorage.getItem('isLoggedIn')) : null
     const { appState } = useContext(AppContext)
     const history = useHistory()
 
-    useEffect(() => {
-        if (!appState.isLoggedIn)
+    useEffect(()=>{
+        if(!appState.isLoggedIn)
             history.push(redirect)
     }, [appState.isLoggedIn]) // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Route {...rest} render={props => (
-            !isLoggedIn ?
-                <Redirect to={redirect} />
-                : <Component {...props} />
+            <Component {...props} />
         )} />
     );
 }
--- src/store/AppStore.js
+++ src/store/AppStore.js
@@ -2,7 +2,7 @@
 // Global store (State) for the app
 
 const appStore = {
-    isLoggedIn: false,
+    isLoggedIn: localStorage.getItem('isLoggedIn') ? JSON.parse(localStorage.getItem('isLoggedIn')) : false,
     currentUser: {},
     loginError: ''
 }
