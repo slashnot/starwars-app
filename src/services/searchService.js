a74d4daad11b3c6dfe493e3c7f035bde035e0d41
Ramkumar K <slashnot84@gmail.com>
2021-01-02 12:40:22 +0530

    Cleanups

--- src/services/searchService.js
+++ src/services/searchService.js
@@ -52,23 +52,6 @@ const searchService = {
         console.log(filtered, rejected)
         return { filtered, rejected }
     },
-
-    // sort_by(field, reverse, primer) {
-
-    //     const key = primer ?
-    //         function (x) {
-    //             return primer(x[field])
-    //         } :
-    //         function (x) {
-    //             return x[field]
-    //         };
-
-    //     reverse = !reverse ? 1 : -1;
-
-    //     return function (a, b) {
-    //         return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
-    //     }
-    // }
 }
 
 export { searchService }
