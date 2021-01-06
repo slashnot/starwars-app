2594a0de0735a497529d87c4886676b7d3e06844
Ramkumar K <slashnot84@gmail.com>
2021-01-06 08:25:34 +0530

    Changed to local api

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 // App configurations
 import planetImages from 'assets/img/images'
 const config = {
-    baseUrl: 'https://starwars-app-krk.netlify.app/api',
+    baseUrl: 'http://localhost:3000/api',
     searchDebounceTime: 400,
     planetImages,
     requestTimeOutDuration: 60000,
